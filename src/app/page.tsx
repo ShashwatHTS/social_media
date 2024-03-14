2bd4c13b11253833dbaec4f70fd8362841cbfab3
Arbaj Ansari <arbaj897ansari@htstechsolutions.com>
2024-03-14 11:20:56 +0530

    s

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 import * as React from "react";
-<<<<<<< HEAD
-import { Moon, Sun } from "lucide-react";
-import { useTheme } from "next-themes";
-// hi
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-=======
 
 import Theme from "@/components/Theme";
->>>>>>> 5dba29464f4cbc75df2324622a5cf6fdac7fc2d1
 export default function Home() {
   return (
     <div className="container">
